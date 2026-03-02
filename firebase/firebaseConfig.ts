7ae24978bc1f666fbf7552afa9c1d69928fba844
Ceren Nur Çetin <cerennurcetinn7@gmail.com>
2026-03-02 16:17:34 +0300

    İlk commit: proje başlangıcı

---
+++ firebase/firebaseConfig.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+//import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfr9swnCG6NPXmlDBOy44U70bk-oycIs0",
+  authDomain: "afetyonetimsistemi-b8a88.firebaseapp.com",
+  projectId: "afetyonetimsistemi-b8a88",
+  storageBucket: "afetyonetimsistemi-b8a88.firebasestorage.app",
+  messagingSenderId: "263355753775",
+  appId: "1:263355753775:web:7854f8ff9fa2b1bd242dda",
+  measurementId: "G-1ZX2WX41WL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+//export default app;
